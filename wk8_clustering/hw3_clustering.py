ba9c0dc9b5465e9112da81f25007b4507a63b1ea
Rob Kimball <kimball.r@me.com>
2021-04-18 16:10:04 -0400

    Adds EMGMM attempt, something broken. First submission=12/25

--- wk8_clustering/hw3_clustering.py
+++ wk8_clustering/hw3_clustering.py
@@ -12,6 +12,29 @@ import scipy as sp
 import pandas as pd
 
 
+def cast_2d_3d(arr, n=2):
+    """
+    Copy data along an axis into a third dimension.
+
+    :param arr: 2D np.array
+    :param n: number of times to copy the axis into the new dimension.
+    :return: 3D np.array
+    """
+    return np.repeat(arr[:, :, np.newaxis], n, axis=2)
+
+
+def random_sample(data, n, axis=0):
+    """
+    Randomly select n rows from a given np.array
+
+    :param data: np.array
+    :param n: int, number of rows to select
+    :param axis: optional, int - the index of the dimension along which to sample.
+    :return: np.array, subset of data
+    """
+    return data[np.random.choice(data.shape[axis], size=n, replace=False)]
+
+
 def save(data, name, ext='csv'):
     """
     Saves a numpy array to as a comma-separated file to the working directory.
@@ -30,10 +53,10 @@ def k_means(data, clusters, iterations):
     :param data: np.ndarray, each row containing the x_i vector of the data to cluster
     :param clusters: int, number of desired clusters to derive from the data
     :param iterations: int, number of times to iterate through the data points to revise the classification.
-    :return: writes a file 'centroids-{iterations}.csv' to the working directory.
+    :return: writes a file 'centroids-{iteration}.csv' to the working directory for each iteration.
     """
     # Randomly initialize the centroids within the data
-    centroids = data[np.random.choice(data.shape[0], size=clusters, replace=False)]
+    centroids = random_sample(data, n=clusters)
 
     for i in range(iterations):
         # Calculate cluster assignments
@@ -46,13 +69,51 @@ def k_means(data, clusters, iterations):
 
 
 def em_gmm(data, clusters, iterations):
+    """
+    Implements the Gaussian Mixture Model clustering algorithm using Expectation-Maximization.
 
-    save('pi-{}'.format(i + 1), pi)
-    save('mu-{}'.format(i + 1), mu)  # this must be done at every iteration
+    :param data: np.ndarray, each row containing the x_i vector of the data to cluster
+    :param clusters: int, number of desired clusters to derive from the data
+    :param iterations: int, number of times to iterate through the data points to revise the classification.
+    :return: writes several files to the working directory for each iteration:
+        pi-{iteration}.csv: the cluster probabilities of the model, each row is the kth probability.
+        mu-{iteration}.csv: the means of each Gaussian, each row is the kth mean.
+        Sigma-{cluster}-{iteration}.csv: the covariance matrix of the Gaussian d*d for the dimensions in data.
+    """
 
-    for j in range(k):  # k is the number of clusters
-        # this must be done 5 times (or the number of clusters) for each iteration
-        save('Sigma-{}-{}'.format(j + 1, i + 1), sigma[j])
+    n, d = data.shape
+    phi = np.zeros((n, clusters))
+    mean = random_sample(data, n=clusters)      # mu, initialized randomly from X
+    prior = (1 / clusters) * np.ones(clusters)  # pi, initialized with the uniform distribution
+    cov = cast_2d_3d(np.eye(d), n=clusters)     # sigma, initialized with the identity matrix
+
+    for i in range(iterations):
+        # Expectation step:
+        for k in range(clusters):
+            inv_cov = np.linalg.inv(cov[:, :, k])
+            sqrt_det = np.linalg.det(cov[:, :, k]) ** -0.5
+            for row in range(n):
+                x_i = data[row, :]
+                _denom = (x_i - mean[k]).T.dot(inv_cov).dot(x_i - mean[k])
+                phi[row, k] = prior[k] * (2 * np.pi)**(-d/2) * sqrt_det * np.exp(_denom * -0.5)
+            # Transform phi into row-wise probabilities
+            phi /= phi.sum(axis=1)[:, np.newaxis]
+
+        # Maximization step:
+        nk = np.sum(phi, axis=0)
+        for k in range(clusters):
+            mean[k] = (phi[:, k].T.dot(data)) / nk[k]
+        for k in range(clusters):
+            t = np.zeros((d, d))
+            for row in range(n):
+                x_i = data[row, :]
+                t += + phi[row, k] * np.outer(x_i - mean[k], x_i - mean[k])
+            cov[:, :, k] = t / nk[k]
+            save(cov[:, :, k], 'Sigma-{}-{}'.format(k + 1, i + 1))
+
+        prior = nk/n
+        save(mean, 'mu-{}'.format(i + 1))
+        save(prior, 'pi-{}'.format(i + 1))
 
 
 if __name__ == '__main__':
@@ -63,7 +124,7 @@ if __name__ == '__main__':
 
     algos = [
         k_means,
-        # em_gmm,
+        em_gmm,
     ]
 
     for algo in algos:
