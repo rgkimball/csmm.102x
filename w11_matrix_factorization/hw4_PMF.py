38ab481c404b916c08ea33c29193e1b70857c55b
Rob Kimball <kimball.r@me.com>
2021-04-25 14:19:42 -0400

    Refactor skeleton code for cleanliness

--- w11_matrix_factorization/hw4_PMF.py
+++ w11_matrix_factorization/hw4_PMF.py
@@ -1,27 +1,32 @@
-from __future__ import division
-import numpy as np
+"""
+Submission for Project 4 of Columbia University's ML EdX course (Probabilistic Matrix Factorization).
+
+    author: @rgkimball
+    date: 4/25/2021
+"""
+
 import sys
-
-train_data = np.genfromtxt(sys.argv[1], delimiter = ",")
-
-lam = 2
-sigma2 = 0.1
-d = 5
-
-# Implement function here
-def PMF(train_data):
- pass
+import numpy as np
 
 
-# Assuming the PMF function returns Loss L, U_matrices and V_matrices (refer to lecture)
-L, U_matrices, V_matrices = PMF(train_data)
+def pmf(data, lam=2, sigma2=0.01, d=5, iterations=50):
+    loss, u, l = None, None, None
+    return loss, u, l
 
-np.savetxt("objective.csv", L, delimiter=",")
 
-np.savetxt("U-10.csv", U_matrices[9], delimiter=",")
-np.savetxt("U-25.csv", U_matrices[24], delimiter=",")
-np.savetxt("U-50.csv", U_matrices[49], delimiter=",")
+def save_pmf_output(loss, u_matrices, v_matrices, iterations):
+    # A comma-separated file containing the PMF objective function along each row
+    np.savetxt('objective.csv', loss, delimiter=',')
+    # A CSV for each iteration specified with the locations corresponding to each row ("user") of M, "U-{i_n}"
+    # and the locations corresponding to the objects of the missing matrix M, "V-{i_n}"
+    for i_n in iterations:
+        np.savetxt('U-{}.csv'.format(i_n), u_matrices[i_n - 1], delimiter=',')
+        np.savetxt('V-{}.csv'.format(i_n), v_matrices[i_n - 1], delimiter=',')
 
-np.savetxt("V-10.csv", V_matrices[9], delimiter=",")
-np.savetxt("V-25.csv", V_matrices[24], delimiter=",")
-np.savetxt("V-50.csv", V_matrices[49], delimiter=",")
+
+if __name__ == '__main__':
+    train_data = np.genfromtxt(sys.argv[1], delimiter=',')
+
+    # Assuming the PMF function returns Loss L, U_matrices and V_matrices (refer to lecture)
+    output = pmf(train_data)
+    save_pmf_output(*output)
