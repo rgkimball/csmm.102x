a118d29e50b726ed2d6a9879df64e6ee2b6ca04d
Rob Kimball <kimball.r@me.com>
2021-04-25 14:09:41 -0400

    Adds skeleton code for PMF assignment

---
+++ w11_matrix_factorization/hw4_PMF.py
@@ -0,0 +1,27 @@
+from __future__ import division
+import numpy as np
+import sys
+
+train_data = np.genfromtxt(sys.argv[1], delimiter = ",")
+
+lam = 2
+sigma2 = 0.1
+d = 5
+
+# Implement function here
+def PMF(train_data):
+ pass
+
+
+# Assuming the PMF function returns Loss L, U_matrices and V_matrices (refer to lecture)
+L, U_matrices, V_matrices = PMF(train_data)
+
+np.savetxt("objective.csv", L, delimiter=",")
+
+np.savetxt("U-10.csv", U_matrices[9], delimiter=",")
+np.savetxt("U-25.csv", U_matrices[24], delimiter=",")
+np.savetxt("U-50.csv", U_matrices[49], delimiter=",")
+
+np.savetxt("V-10.csv", V_matrices[9], delimiter=",")
+np.savetxt("V-25.csv", V_matrices[24], delimiter=",")
+np.savetxt("V-50.csv", V_matrices[49], delimiter=",")
